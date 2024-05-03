7b1b848f5effa9e06e785af71b1ec1e7507c162b
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-03 14:42:44 +0300

    feat: add button to logout and add logic

---
+++ src/features/auth/components/LogoutUser.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-btn @click="onClick">Logout</q-btn>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '../store/authStore';
+
+const authStore = useAuthStore();
+
+function onClick() {
+  authStore.userLogout();
+}
+</script>
