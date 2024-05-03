8dd28865a81898ddb3e74be27b6fd2ea4bf1a148
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-03 18:53:23 +0300

    feat: add event message for webSocket, interface for Data from webSocket and add types

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,30 @@
 </template>
 
 <script setup lang="ts">
-defineOptions({
-  name: 'App',
-});
+import { useAuthStore } from './features/auth/store/authStore';
+import { ws } from './shared/api/ws';
+
+interface Data {
+  id: string | null,
+  type: string,
+  payload: any
+}
+
+interface Types {
+  [key: string]: () => void
+}
+
+const authStore = useAuthStore();
+
+const types: Types = {
+  USER_LOGIN: authStore.handleUserLogin,
+  USER_LOGOUT: authStore.handleUserLogout,
+}
+
+ws.addEventListener('message', (event) => {
+  event.preventDefault();
+  const data: Data = JSON.parse(event.data);
+  const action = types[data.type];
+  action();
+})
 </script>
