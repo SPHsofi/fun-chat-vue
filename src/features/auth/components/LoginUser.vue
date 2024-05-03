73aa498c87d4b3b8f87501d7ef188215abe7bb78
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-03 18:52:22 +0300

    refactor: change form tag, delete quasar checks for inputs and change logic checks

--- src/features/auth/components/LoginUser.vue
+++ src/features/auth/components/LoginUser.vue
@@ -1,33 +1,20 @@
 <template>
-  <form class="form">
-    <q-input
-      v-model="login"
-      label="Login"
-      error-message="Please use maximum 4 characters"
-      :error="!validation.isLoginValid"
-    />
-    <q-input
-    v-model="password"
-    label="password"
-    error-message="Please use maximum 6 characters"
-    :error="!validation.isPassword"
-    />
+  <q-form @submit.prevent="onSubmit" class="form">
+    <q-input v-model="authStore.authData.login" label="Login" />
+    <q-input v-model="authStore.authData.password" label="pass  word" />
     <q-btn type="submit">Login</q-btn>
-  </form>
+  </q-form>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { useAuthStore } from '../store/authStore';
 
-const login = ref('');
-const password = ref('');
+const authStore = useAuthStore();
 
-const validation = computed(() => {
-  return {
-    isLoginValid: login.value.length >= 4,
-    isPassword: password.value.length >= 6,
-  };
-});
+function onSubmit() {
+  authStore.userLogin();
+}
+
 </script>
 
 <style scoped>
