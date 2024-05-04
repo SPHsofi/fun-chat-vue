eb1137c1d8cc1d88949b1d20e818dfe0e0e83c67
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-04 11:15:55 +0300

    refactor: fix grammar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
-          {{ authStore.authData.login }}`s Messanger
+          {{ authStore.authData.login }}`s Messenger
         </q-toolbar-title>
 
       <LogoutUser />
